71c6a937c7ee1231f7f6a0e484b2d042d88ef461
Oscar <szkrtests@gmail.com>
2023-12-04 21:10:07 +0100

    Changed a lot and added every testing enviroment.

---
+++ wip/replacesrc/script.js
@@ -0,0 +1,23 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const elementToShow = document.querySelector('.main-text-3');
+//     setTimeout(() => {
+//       elementToShow.style.visibility = 'visible';
+//       elementToShow.style.opacity = '0';
+//       requestAnimationFrame(() => {
+//         elementToShow.style.transition = 'opacity 1s ease-in-out';
+//         elementToShow.style.opacity = '1';
+//       });
+//     }, 2000);
+//   });
+// document.addEventListener('DOMContentLoaded', () => {
+//     const textToClick = document.getElementById('video-player-starter-text-3');
+//     const divToHide1 = document.getElementById('.main-text');
+//     const divToHide2 = document.getElementById('.main-text-2');
+//     const videoContainer = document.getElementById('videoContainer');
+  
+//     textToClick.addEventListener('click', () => {
+//       divToHide1.style.display = 'none';
+//       divToHide2.style.display = 'none';
+//       videoContainer.style.display = 'block';
+//     });
+// });
